f4f82f4d0745d3e35609f2fb02e7e8d28a62adf9
PaolaTorres922 <dannapaolavegatorres@gmail.com>
2024-12-02 11:51:21 -0600

    Update games.js

--- src/stores/games.js
+++ src/stores/games.js
@@ -1,26 +1,68 @@
-import { defineStore } from 'pinia'
-
-export const useGamesStore = defineStore('games', {
-  state: () => ({
+const games = {
+  strategy: {
+    title: 'JUEGOS DE ESTRATEGIA',
+    backgroundColor: '#FDE47F',
     games: [
-      { id: 1, name: 'Ajedrez', price: 29.99, image: 'https://m.media-amazon.com/images/I/61LA5e7uHtL._AC_UL320_.jpg' },
-      { id: 2, name: 'Damas', price: 19.99, image: 'https://m.media-amazon.com/images/I/61RQnxpsOVL._AC_UL320_.jpg' },
-      { id: 3, name: 'Monopoly', price: 39.99, image: 'https://m.media-amazon.com/images/I/81T7hKpWNNL.__AC_SX300_SY300_QL70_ML2_.jpg' },
-      { id: 4, name: 'Scrabble', price: 34.99, image: 'https://m.media-amazon.com/images/I/811cnzGFDDL._AC_UL320_.jpg' },
-      { id: 5, name: 'Connect 4', price: 24.99, image: 'https://m.media-amazon.com/images/I/819ZLc1npzL._AC_UL320_.jpg' },
-      { id: 6, name: 'Hedbanz', price: 22.99, image: 'https://m.media-amazon.com/images/I/81ogvpoYu2L._AC_UL320_.jpg' },
-      { id: 7, name: 'UNO', price: 14.99, image: 'https://m.media-amazon.com/images/I/8121iJtKLRL._AC_UL320_.jpg' },
-      { id: 8, name: 'Phase 10', price: 19.99, image: 'https://m.media-amazon.com/images/I/71ISPZrjeSL._AC_UL320_.jpg' }
-    ],
-    categories: [
-      'JUEGOS DE CARTAS',
-      'JUEGOS DE ESTRATEGIA',
-      'JUEGOS DE PARTY',
-      'JUEGOS DE FAMILIA'
+      { id: 1, name: 'Ajedrez', price: 1135, image: '/src/assets/ajedrez.jpg' },
+      { id: 2, name: 'Damas', price: 1219, image: '/src/assets/damas.jpg' },
+      { id: 3, name: 'Monopoly', price: 1658, image: '/src/assets/monopoly.jpg' },
+      { id: 4, name: 'Jumanji', price: 1079, image: '/src/assets/jumanji.jpg' },
+      { id: 5, name: '4 en Linea', price: 1318, image: '/src/assets/cuatro-en-linea.jpg' },
+      { id: 6, name: 'Risk', price: 1499, image: '/src/assets/risk.jpg' },
+      { id: 7, name: 'Scrabble', price: 1259, image: '/src/assets/scrabble.jpg' },
+      { id: 8, name: 'Catan', price: 1899, image: '/src/assets/catan.jpg' },
+      { id: 9, name: 'Clue', price: 1399, image: '/src/assets/clue.jpg' },
+      { id: 10, name: 'Stratego', price: 1459, image: '/src/assets/stratego.jpg' }
     ]
-  }),
-  getters: {
-    getAllGames: (state) => state.games,
-    getAllCategories: (state) => state.categories
+  },
+  cards: {
+    title: 'JUEGOS DE CARTAS',
+    backgroundColor: '#A8D08D',
+    games: [
+      { id: 11, name: 'Hedbanz', price: 998, image: '/src/assets/hedbanz.jpg' },
+      { id: 12, name: 'Uno flip', price: 1120, image: '/src/assets/uno-flip.jpg' },
+      { id: 13, name: 'Cartas Royal', price: 1346, image: '/src/assets/royal.jpg' },
+      { id: 14, name: 'Baraja Española', price: 185, image: '/src/assets/cartas-españolas.jpg' },
+      { id: 15, name: 'Virus', price: 638, image: '/src/assets/virus.jpg' },
+      { id: 16, name: 'Phase 10', price: 899, image: '/src/assets/phase-diez.jpg' },
+      { id: 17, name: 'Skip-Bo', price: 759, image: '/src/assets/skip-bo.jpg' },
+      { id: 18, name: 'Solitario', price: 499, image: '/src/assets/solitario.jpg' },
+      { id: 19, name: 'Poker Set', price: 2499, image: '/src/assets/poker-set.jpg' },
+      { id: 20, name: 'Dobble', price: 699, image: '/src/assets/dobble.jpg' }
+    ]
+  },
+  party: {
+    title: 'JUEGOS DE PARTY',
+    backgroundColor: '#FFB84D',
+    games: [
+      { id: 21, name: 'Party 6', price: 1198, image: '/src/assets/party-seis.jpg' },
+      { id: 22, name: '100 Mexicanos dijeron', price: 1129, image: '/src/assets/cien-mexicanos-dijeron.jpg' },
+      { id: 23, name: 'Uno party', price: 1028, image: '/src/assets/uno-party.jpg' },
+      { id: 24, name: 'Torre del beber', price: 1185, image: '/src/assets/torre-del-beber.jpg' },
+      { id: 25, name: 'Me caigo de risa', price: 1624, image: '/src/assets/me-caigo-de-risa.jpg' },
+      { id: 26, name: 'Twister', price: 899, image: '/src/assets/twister.jpg' },
+      { id: 27, name: 'Pictionary', price: 1299, image: '/src/assets/pictionary.jpg' },
+      { id: 28, name: 'Cranium', price: 1599, image: '/src/assets/cranium.jpg' },
+      { id: 29, name: 'Taboo', price: 999, image: '/src/assets/taboo.jpg' },
+      { id: 30, name: 'Charadas', price: 799, image: '/src/assets/charadas.jpg' }
+    ]
+  },
+  family: {
+    title: 'JUEGOS DE FAMILIA',
+    backgroundColor: '#B3E5FC',
+    games: [
+      { id: 31, name: 'Quien es quien?', price: 1998, image: '/src/assets/adivina-quien.jpg' },
+      { id: 32, name: 'Basta', price: 1159, image: '/src/assets/basta.jpg' },
+      { id: 33, name: 'Preguntados', price: 1276, image: '/src/assets/preguntados.jpg' },
+      { id: 34, name: 'Gestos', price: 1015, image: '/src/assets/gestos.jpg' },
+      { id: 35, name: 'Jenga', price: 994, image: '/src/assets/jenga.jpg' },
+      { id: 36, name: 'Serpientes y Escaleras', price: 599, image: '/src/assets/serpientes-y-escaleras.jpg' },
+      { id: 37, name: 'Operando', price: 1199, image: '/src/assets/operando.jpg' },
+      { id: 38, name: 'Adivina Quien', price: 899, image: '/src/assets/adivina-quien.jpg' },
+      { id: 39, name: 'Lotería', price: 499, image: '/src/assets/loteria.jpg' },
+      { id: 40, name: 'Memorama', price: 399, image: '/src/assets/memorama.jpg' }
+    ]
   }
-})
+}
+
+export default games
